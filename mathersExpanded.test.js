32c86349e6daa2929fa061b20d2eb82d35966e26
Nadim Saker <nadimsaker@gmail.com>
2019-11-27 17:12:16 +0600

    Number Matcher

--- mathersExpanded.test.js
+++ mathersExpanded.test.js
@@ -1,3 +1,15 @@
 test("there is pool in Liverpool", () => {
   expect("Liverpool").toMatch(/pool/);
 });
+
+const multiply = require("./multiply");
+
+describe("test multiply positive scenarios", () => {
+  test("multiply 3*2 should equal to 6", () => {
+    expect(multiply(3, 2)).toBe(6);
+    expect(multiply(3, 2)).toBeGreaterThan(5);
+    expect(multiply(3, 2)).toBeLessThan(7);
+    expect(multiply(3, 2)).toBeGreaterThanOrEqual(6);
+    expect(multiply(3, 2)).toBeLessThanOrEqual(6);
+  });
+});
